f6a8adc5b8f795fc181930c7f57766178a44b816
amandabenevides <benevidesamanda13@gmail.com>
2020-10-30 20:01:56 -0300

    Updates

---
+++ precache-manifest.affe07a5e07fcfed97f552d0a35cad1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da3ccdb340cfa5235a5e05fc6fc56ee",
+    "url": "/amandabenevides/poke-list/index.html"
+  },
+  {
+    "revision": "e18a331a959573b6bf2f",
+    "url": "/amandabenevides/poke-list/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "00a9175f3065dd62dbab",
+    "url": "/amandabenevides/poke-list/static/css/main.33962e36.chunk.css"
+  },
+  {
+    "revision": "e18a331a959573b6bf2f",
+    "url": "/amandabenevides/poke-list/static/js/2.4c524ffb.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/amandabenevides/poke-list/static/js/2.4c524ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a9175f3065dd62dbab",
+    "url": "/amandabenevides/poke-list/static/js/main.011e44d3.chunk.js"
+  },
+  {
+    "revision": "09a60978b0c0d383dfb5",
+    "url": "/amandabenevides/poke-list/static/js/runtime-main.730e0313.js"
+  }
+]);
